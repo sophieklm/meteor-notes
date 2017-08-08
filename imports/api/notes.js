f75af9eb1960645232dbf61f1e4af550ade078f2
sophieklm <sklm@live.co.uk>
2017-08-08 21:21:09 +0100

    adds note remove

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import moment from 'moment';
+import SimpleSchema from 'simpl-schema';
 
 export const Notes = new Mongo.Collection('notes');
 
@@ -14,6 +15,21 @@ Meteor.methods({
       body: '',
       userId: this.userId,
       updatedAt: moment().valueOf()
+    });
+  },
+  'notes.remove'(_id) {
+    if(!this.userId) {
+      throw new Meteor.Error('not-authorised');
+    }
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({_id});
+    return Notes.remove({
+      _id,
+      userId: this.userId
     });
   }
 });
--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -4,15 +4,34 @@ import { Notes } from './notes';
 
 if(Meteor.isServer) {
   describe('notes', function() {
-    it('should work when user logged in', function() {
+
+    beforeEach(function() {
+      Notes.remove({});
+      Notes.insert({
+        _id: 'testNoteId1',
+        title: 'Title',
+        body: 'Content',
+        updatedAt: 0,
+        userId: 'testUserId1'
+      });
+    });
+
+    it('should insert a new note', function() {
       const userId = 'testid';
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
       expect(Notes.findOne({ _id, userId })).toExist();
     });
-    it('should fail when user not logged in', function() {
+
+    it('should note insert a new note when user not logged in', function() {
       expect(() => {
         Meteor.server.method_handlers['notes.insert']();
       }).toThrow();
     });
+
+    it('should remove a note', function() {
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+      expect(Notes.findOne({ _id: 'testNoteId1' })).toNotExist();
+    });
+
   });
 }
