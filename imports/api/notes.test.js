0f26062af0fb31055942fdf358a5eb3e39679363
sophieklm <sklm@live.co.uk>
2017-08-08 21:55:50 +0100

    updates notes tests

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -12,9 +12,18 @@ if(Meteor.isServer) {
       userId: 'testUserId1'
     };
 
+    const noteTwo = {
+      _id: 'testNoteId2',
+      title: 'Another Title',
+      body: 'Some List',
+      updatedAt: 0,
+      userId: 'testUserId2'
+    };
+
     beforeEach(function() {
       Notes.remove({});
       Notes.insert(noteOne);
+      Notes.insert(noteTwo);
     });
 
     it('should insert a new note', function() {
@@ -90,6 +99,19 @@ if(Meteor.isServer) {
       expect(() => {
         Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId });
       }).toThrow();
+    });
+
+    it('should return a users notes', function() {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: noteOne.userId });
+      const notes = res.fetch();
+      expect(notes.length).toBe(1);
+      expect(notes[0]).toEqual(noteOne);
+    });
+
+    it('should return zero notes for a user with no notes', function() {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: 'randomuser' });
+      const notes = res.fetch();
+      expect(notes.length).toBe(0);
     });
 
   });
