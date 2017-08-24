c6c6921968d23fddb7a47f00d9b21a6ff8155348
sophieklm <sklm@live.co.uk>
2017-08-24 12:16:59 +0100

    adds login tests

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -12,7 +12,35 @@ if(Meteor.isClient) {
       const wrapper = mount(<Login loginWithPassword={() => {}} />);
 
       wrapper.setState({ error });
-      expect(wrapper.find('p').text()).toBe(error);      
+      expect(wrapper.find('p').text()).toBe(error);
+    });
+
+    it('should call loginWithPassword with form data', function() {
+      const email = 'test@test.com';
+      const password = 'password';
+      const spy = expect.createSpy();
+      const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+      wrapper.ref('email').node.value = email;
+      wrapper.ref('password').node.value = password;
+
+      wrapper.find('form').simulate('submit');
+
+      expect(spy.calls[0].arguments[0]).toEqual({ email });
+      expect(spy.calls[0].arguments[1]).toEqual(password);
+    });
+
+    it('should set loginWithPassword callback errors', function() {
+      const spy = expect.createSpy();
+      const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+      wrapper.find('form').simulate('submit');
+
+      spy.calls[0].arguments[2]({});
+      expect(wrapper.state('error')).toNotBe('');
+
+      spy.calls[0].arguments[2]();
+      expect(wrapper.state('error').length).toBe(0);
     });
   });
 }
