3ca28c80d923e0263e5c8bafad4826828ed87b03
sophieklm <sklm@live.co.uk>
2017-08-26 11:36:11 +0100

    adds selected note id

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
+import { browserHistory } from 'react-router';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
 import '../imports/startup/simpl-schema-config.js';
@@ -10,6 +11,14 @@ import '../imports/startup/simpl-schema-config.js';
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
+
+  if(selectedNoteId) {
+    browserHistory.replace(`/dashboard/${selectedNoteId}`);
+  }
 });
 
 Meteor.startup(() => {
