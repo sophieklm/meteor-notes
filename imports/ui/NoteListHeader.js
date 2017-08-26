2e5a7bd1415d06da83ba4fb2033cedea91ac1670
sophieklm <sklm@live.co.uk>
2017-08-26 13:45:30 +0100

    sorts notes and selects new note

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return (
     <div>
       <button className="button" onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert', (err, res) => {
+          if (res) {
+            props.Session.set('selectedNoteId', res);
+          }
+        });
       }}>Create Note</button>
     </div>
   );
 };
 
+NoteListHeader.propTypes = {
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
+};
+
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader);
