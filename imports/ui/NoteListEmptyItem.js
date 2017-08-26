e986d77413110f65c80b29b06919406eff915544
sophieklm <sklm@live.co.uk>
2017-08-26 10:55:11 +0100

    adds note list empty item message

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h5>You have no notes</h5>
+      <p>Click the Create Note button to get started</p>
+    </div>
+  );
+};
