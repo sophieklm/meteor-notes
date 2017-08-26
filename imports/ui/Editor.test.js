2ccbc7cf4e9447657ec59d73e63df1145b1c2ae9
sophieklm <sklm@live.co.uk>
2017-08-26 13:33:08 +0100

    adds editor test cases

--- imports/ui/Editor.test.js
+++ imports/ui/Editor.test.js
@@ -37,5 +37,54 @@ if(Meteor.isClient) {
       expect(browserHistory.push).toHaveBeenCalledWith('/dashboard');
       expect(call).toHaveBeenCalledWith('notes.remove', notes[0]._id);
     });
+
+    it('should update note body on textarea change', function() {
+      const newBody = 'Some new text';
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call} selectedNoteId={notes[0]._id} note={notes[0]}/>);
+
+      wrapper.find('textarea').simulate('change', {
+        target: {
+          value: newBody
+        }
+      });
+      expect(wrapper.state('body')).toBe(newBody);
+      expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, { body: newBody });
+    });
+
+    it('should update note title on input change', function() {
+      const newTitle = 'Some new title';
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call} selectedNoteId={notes[0]._id} note={notes[0]}/>);
+
+      wrapper.find('input').simulate('change', {
+        target: {
+          value: newTitle
+        }
+      });
+      expect(wrapper.state('title')).toBe(newTitle);
+      expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, { title: newTitle });
+    });
+
+    it('should set state for new note', function() {
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call}/>);
+
+      wrapper.setProps({
+        selectedNoteId: notes[0]._id,
+        note: notes[0]
+      });
+
+      expect(wrapper.state('title')).toBe(notes[0].title);
+      expect(wrapper.state('body')).toBe(notes[0].body);
+    });
+
+    it('should not set state if note prop not provided', function() {
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call}/>);
+
+      wrapper.setProps({
+        selectedNoteId: notes[0]._id
+      });
+
+      expect(wrapper.state('title')).toBe('');
+      expect(wrapper.state('body')).toBe('');
+    });
   });
 }
